6cbf10e6c2caf7cc1df840a7b770bfe2cbf162c9
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2023-04-09 05:41:02 +0700

    register Update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require("path")
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const loginRouter = require('./routes/login')
 const projectRouter = require('./routes/project')
--- routes/login.js
+++ routes/login.js
@@ -12,4 +12,28 @@ router.get('/register', (req, res) => {
     res.render('register', data)
 })
 
+
+router.post("/register", async function (req, res, next) {
+    console.log(req.body)
+    const username = req.body.username;
+    const password = req.body.password;
+    const conn = await pool.getConnection()
+    // Begin transaction
+    await conn.beginTransaction();
+    try {
+        const [rows] = await conn.query("SELECT * FROM accounts WHERE username = ?", [username]);
+        if (rows.length > 0) {
+            res.render('register', {alert: "มีชื่อผู้ใช้งานนี้แล้ว"})
+        } else {
+            await conn.query("INSERT INTO accounts(username, password, permission) VALUES(?, ?, 'customer');", [username, password]);
+            await conn.commit();
+            res.redirect('/login');
+        }
+    } catch (err) {
+        await conn.rollback();
+    } finally {
+        conn.release();
+    }
+  });
+
 module.exports = router
